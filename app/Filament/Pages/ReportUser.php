5235932317a5886ad45910dd6b564ad85a101ac2
myrole656 <122355717+myrole656@users.noreply.github.com>
2025-11-29 08:31:18 +0800

    push

--- app/Filament/Pages/ReportUser.php
+++ app/Filament/Pages/ReportUser.php
@@ -3,12 +3,14 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use App\Filament\Widgets\UserInfo;
 use App\Filament\User\Widgets\ProjectDropDown;
 use App\Filament\User\Widgets\UserProjectStats;
 use App\Filament\User\Widgets\ResultChart;
 use App\Filament\User\Widgets\TargetProject;
 use App\Filament\User\Widgets\SDG;
 use App\Filament\User\Widgets\OpenPdfWidget;
+use UnitEnum; 
 use BackedEnum;
 
 class ReportUser extends Page
@@ -16,13 +18,7 @@ class ReportUser extends Page
     protected string $view = 'filament.pages.report-user';
     protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-users';
 
-    // FIXED signature
-    public function getColumns(): array | int
-    {
-        return 12;
-    }
-
-    protected function getHeaderWidgets(): array
+     protected function getHeaderWidgets(): array
     {
         return [
             ProjectDropDown::class,
@@ -34,12 +30,5 @@ class ReportUser extends Page
         ];
     }
 
-    // Correct header widget span function
-    public function getHeaderWidgetsColumnSpan(): array
-    {
-        return [
-            ProjectDropDown::class => 6,
-            OpenPdfWidget::class => 6,
-        ];
-    }
+    
 }
--- app/Filament/User/Pages/Dashboard.php
+++ app/Filament/User/Pages/Dashboard.php
@@ -9,33 +9,23 @@ use App\Filament\User\Widgets\UserProjectStats;
 use App\Filament\User\Widgets\TargetProject;
 use App\Filament\User\Widgets\OpenPdfWidget;
 
+
 class Dashboard extends BaseDashboard
 {
     protected static ?string $title = 'Dashboard';
 
-    // FIXED signature
-    public function getColumns(): array | int
-    {
-        return 12;
-    }
-
     public function getWidgets(): array
     {
         return [
+            
             ProjectDropDown::class,
             OpenPdfWidget::class,
             UserProjectStats::class,
             ResultChart::class,
             TargetProject::class,
+
         ];
     }
 
-    // Column spans for dashboard widgets
-    public function getWidgetColumnSpan(): array
-    {
-        return [
-            ProjectDropDown::class => 6,
-            OpenPdfWidget::class => 6,
-        ];
-    }
+    
 }
